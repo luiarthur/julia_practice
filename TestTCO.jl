73305a7bd4e949690cb0f9a4d617ed6e31c8c368
Arthur Lui <luiarthur@gmail.com>
2016-05-27 16:16:53 -0700

    yes

--- TestTCO.jl
+++ TestTCO.jl
@@ -4,6 +4,8 @@ using Base.Test
 function TestTCO()
   numPass = 0
   numFail = 0
+  passColor = :cyan
+  failColor = :red
 
   #=
     write a macro @vtest (verbose test) that does this
@@ -12,13 +14,13 @@ function TestTCO()
   =#
 
   function custom_handler(r::Test.Success)
-    print_with_color(:green,"Success on test: $(r.expr)\n")
-    numPass = numPass + 1
+    print_with_color(passColor, "Success on test: $(r.expr)\n")
+    numPass += 1
   end
 
   function custom_handler(r::Test.Failure) 
-    print_with_color(:red,"Error on test : $(r.expr)\n")
-    numFail = numPass + 1
+    print_with_color(failColor, "Error on test : $(r.expr)\n")
+    numFail += 1
   end
 
   function custom_handler(r::Test.Error) 
@@ -32,9 +34,18 @@ function TestTCO()
     @test logfact(3) == log(6)
     @test logfact_woTCO(3) == log(6)
     @test 1 == 2
+    @test let
+      x = 1 == 1
+      x
+    end
+    @test let
+      x = 1 == 2
+      x
+    end
     # END OF TESTS:
 
-    print_with_color(:green, 
+    printColor = numFail == 0 ? passColor : failColor
+    print_with_color(printColor, 
       string("Tests Passed: ", numPass, " of ", numFail + numPass),"\n")
   end
 end
@@ -43,4 +54,3 @@ end
   include("TestTCO.jl")
   TestTCO()
 =#
-
